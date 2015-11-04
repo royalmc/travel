58811593772942f3ccd2942a69920c8a583e00f5
Royal <iroyal@gmail.com>
2015-11-03 20:56:15 -0500

    Added contact page

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,13 +1,14 @@
 class WelcomeController < ApplicationController
   def index
-    @homeland = 'Memphis'
-    @countries = ['Tokyo', 'London', 'Maui', 'Johannesburg']
-    @pictures = ['tokyo.jpg', 'london.jpg', 'maui.jpg', 'johannesburg.jpg']
+  	@homeland = 'Puerto Rico'
+    @countries = ['Tokyo','London','Maui', 'Johannesburg']
+    @pictures = ["tokyo.jpg", "London.jpg", "maui.jpg", "johannesburg.jpg"]
   end
 
   def about
-  	@color = params[:color]
-  	@size = params[:size].to_i
- 	end	
-
+    @color = params[:color]
+    @size = params[:size].to_i
+  end
+  def contact
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   root 'welcome#index'
 
-  get "about" => 'welcome#about'
+  get 'about' => 'welcome#about'
+
+  get 'contact' => 'welcome#contact'
 
 
   # The priority is based upon order of creation: first created -> highest priority.
